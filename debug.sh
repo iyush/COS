139e0b846108940699e87f57ecbe015c7eabb164
aayush <aayushsharma889@gmail.com>
2024-08-04 12:30:12 +0200

    add a debug command

--- debug.sh
+++ debug.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
 
 ./build.sh
-qemu-system-x86_64 -cdrom template.iso -s -S -m 256 -serial stdio 
+qemu-system-x86_64 -cdrom template.iso -s -S -m 256 -serial stdio &
+./toolchain/src/gf/gf2 build/kernel -x kernel/init.gdb &
+wait
