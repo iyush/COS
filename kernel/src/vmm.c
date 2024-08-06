22ecde2b6b66061dabaf82bb30b9e53dff2bb863
aayush <aayushsharma889@gmail.com>
2024-08-06 18:27:26 +0200

    vmm skeleton

--- kernel/src/vmm.c
+++ kernel/src/vmm.c
@@ -1,6 +1,14 @@
 #include <pmm.h>
 #include <stdint.h>
 #include <limine.h>
+#include <mem.h>
+
+extern uint64_t _KERNEL_START;
+extern uint64_t _KERNEL_END;
+
+#define RECURSIVE_PAGE_TABLE_ENTRY_OFFSET 510
+
+uint64_t pml4_table[512][512] __attribute__((aligned (4096)));
 
 uint64_t _vmm_cr3()
 {
@@ -15,7 +23,78 @@ uint64_t _vmm_cr3()
     return cr3;
 }
 
-void vmm_init(struct limine_hhdm_request hhdm_request)
+void panic(char * msg)
+{
+    ksp("PANIC!!!!!!\n\t %s\n", msg);
+    while(1) {}
+}
+
+void * vmm_get_pdpte(uint64_t* pml4e_table, uint64_t pml4e_offset)
+{
+    return 
+
+}
+
+void vmm_map_page(uint64_t* pml4e_table, void * v_start, void* p_start)
+{
+    if (((uint64_t)p_start & 0xfff) != 0)
+    {
+        panic("vmm_map_page: p_start is not a valid 4kb physical page address.");
+    }
+    if (((uint64_t)v_start & 0xfff) != 0)
+    {
+        panic("vmm_map_page: v_start is not a valid 4kb virtual page address.");
+    }
+
+    uint64_t pml4e_offset = (uint64_t)(((uint64_t)v_start >> 39) & 0x01ff);
+    uint64_t pdpte_offset = (uint64_t)(((uint64_t)v_start >> 30) & 0x01ff);
+    uint64_t pde_offset   = (uint64_t)(((uint64_t)v_start >> 21) & 0x01ff);
+    uint64_t pte_offset   = (uint64_t)(((uint64_t)v_start >> 12) & 0x01ff);
+
+    if (!(pml4e_table[pml4e_offset] & PAGE_PRESENT))
+    {
+        pml4e_table[pml4e_offset] = (uint64_t)(pmm_alloc_page(1)) | PAGE_PRESENT | PAGE_WRITABLE;
+    }
+    uint64_t * pdpte_table = vmm_get_pdpte(pml4e_table, pml4e_offset); // we can't do this as the page table is not in the cr4.
+
+    uint64_t * pd_table;
+    if (pdpte_table[pdpte_offset] & PAGE_PRESENT)
+    {
+        pd_table = (uint64_t *)((pdpte_table[pdpte_offset] >> 12) * 4096);
+    }
+    else
+    {
+        pd_table = pmm_alloc_page(1);
+        pdpte_table[pdpte_offset] = (uint64_t)(pd_table) | PAGE_PRESENT | PAGE_WRITABLE; 
+    }
+
+    uint64_t * p_table;
+    if (pd_table[pde_offset] & PAGE_PRESENT)
+    {
+        p_table = (uint64_t *)((pd_table[pde_offset] >> 12) * 4096);
+    }
+    else
+    {
+        p_table = pmm_alloc_page(1);
+        pd_table[pde_offset] = (uint64_t)(p_table) | PAGE_PRESENT | PAGE_WRITABLE; 
+    }
+
+    p_table[pte_offset] = (uint64_t)p_start | PAGE_PRESENT | PAGE_WRITABLE;
+}
+
+void vmm_init(struct limine_hhdm_request hhdm_request, struct limine_kernel_address_request kadd_request)
+{
+    (void)hhdm_request;
+    uint64_t kernel_code_size_in_pages = ((uint64_t)&_KERNEL_END - (uint64_t)&_KERNEL_START + 4096) / 4096;
+
+    uint64_t * p4_table = pml4_table[0];
+    p4_table[RECURSIVE_PAGE_TABLE_ENTRY_OFFSET] = ((uint64_t)p4_table | PAGE_PRESENT | PAGE_WRITABLE);
+    vmm_map_page(p4_table, (uint64_t *)KERNEL_TEXT_VMA, (uint64_t *)kadd_request.response->physical_base);//, kernel_code_size_in_pages);
+
+    ksp("%lx %lx %lx kernel_code_size: %lx\n",  _KERNEL_START, (uint64_t)&_KERNEL_START, kadd_request.response->physical_base, kernel_code_size_in_pages);
+}
+
+void vmm_pg_dmp(struct limine_hhdm_request hhdm_request) 
 {
     uint64_t *p4_table = (uint64_t *)(hhdm_request.response->offset + _vmm_cr3()); //pml4
     for (int p4idx = 0; p4idx < 512; p4idx++)
