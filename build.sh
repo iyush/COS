0ce349709ea921c75891b7bee06d90e7cbe20fa3
Aayush <aayushsharma889@gmail.com>
2024-12-15 00:23:24 +0100

    move fully to nix shell, yeet out the toolchain

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 set -e
-TOOLCHAIN_DIR=./toolchain/build
-PATH=./toolchain/build/bin:$PATH
 
 rm -rf build/
 mkdir -p build/
 
-x86_64-elf-gcc -g -pipe -Wall -Wextra -Werror -Wno-missing-field-initializers -std=gnu11 -ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fPIE -m64 -march=x86-64 -mno-80387 -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -I kernel/src -I limine/  -MMD -MP -c kernel/src/entry.c -o build/entry.c.o
+x86_64-elf-gcc -g -pipe -Wall -Wextra -Werror -Wno-missing-field-initializers -std=gnu11 -ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fPIE  -march=x86-64 -mno-80387 -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -I kernel/src -I limine/  -MMD -MP -c kernel/src/entry.c -o build/entry.c.o
 nasm -F dwarf -g -f elf64 kernel/src/idt.asm -o build/idt.asm.o
 
-ld build/entry.c.o build/idt.asm.o  -m elf_x86_64 -nostdlib -pie -z text -z max-page-size=0x1000 -T kernel/linker.ld -o build/kernel
+x86_64-elf-ld build/entry.c.o build/idt.asm.o  -m elf_x86_64 -nostdlib -pie -z text -z max-page-size=0x1000 -T kernel/linker.ld -o build/kernel
 printf '\x03' | dd of=build/kernel bs=1 count=1 seek=16 conv=notrunc 2> /dev/null
 
 rm -rf iso_root
@@ -21,9 +19,10 @@ cp limine.cfg limine/limine-bios.sys limine/limine-bios-cd.bin limine/limine-uef
 mkdir -p iso_root/EFI/BOOT
 cp limine/BOOTX64.EFI iso_root/EFI/BOOT/
 cp limine/BOOTIA32.EFI iso_root/EFI/BOOT/
-x86_64-elf-xorriso -as mkisofs -b boot/limine/limine-bios-cd.bin \
+xorriso -as mkisofs -b boot/limine/limine-bios-cd.bin \
         -no-emul-boot -boot-load-size 4 -boot-info-table \
         --efi-boot boot/limine/limine-uefi-cd.bin \
         -efi-boot-part --efi-boot-image --protective-msdos-label \
         iso_root -o template.iso 2> /dev/null
 ./limine/limine bios-install template.iso 2> /dev/null
+echo "Finished building!"
--- debug.sh
+++ debug.sh
@@ -3,5 +3,5 @@ set -e
 
 ./build.sh
 qemu-system-x86_64 -cdrom template.iso -s -S -m 256 -serial stdio &
-gdb build/kernel -x kernel/init.gdb
+x86_64-elf-gdb build/kernel -x kernel/init.gdb
 wait
